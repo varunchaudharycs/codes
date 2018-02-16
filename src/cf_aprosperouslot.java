7de24d1f147c87a2951e198bcc92528d62355600
Prateek <varchd250295@gmail.com>
2018-02-16 14:55:53 +0530

    Corrrected file name

--- src/cf_aprosperouslot.java
+++ src/cf_aprosperouslot.java
@@ -1,22 +1,25 @@
 import java.io.*;
-import java.math.*;
 
-public class cf_aprosperouslot
+class cf_aprosperouslot
 {
-	public static void main(String args[]) throws IOException
-	{
-		BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
-		
-		int k = Integer.parseInt(buf.readLine());
-		
-		BigInteger n = new BigInteger("1");
-		BigInteger base = new BigInteger("10");
-		base = base.pow(k);
-		base = base.subtract(n);
-						
-		n = n.multiply(base);
-		
-		System.out.println(n);
-		
-	}
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+
+        int k = Integer.parseInt(buf.readLine());
+        int n = k / 2, i;
+
+        if(k > 36)
+        {
+            System.out.print("-1");
+            System.exit(0);
+        }
+
+        for(i = 1; i <= n; i++)
+            System.out.print(8);
+
+        if(k % 2 == 1)
+            System.out.print(4);
+
+    }
 }
