d02e2c679495d1194ca60f61e401dc5bd1dde283
Varun Chaudhary <varchd250295@gmail.com>
2019-06-12 12:09:46 +0530

    Update lc_twosum.java

--- src/lc_twosum.java
+++ src/lc_twosum.java
@@ -1,19 +1,10 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // soln. array of indices
-        int ans[] = new int[2];
-        // length of array = no. of elements
-        int len = nums.length;
-        boolean found = false;
         // iterate over array, pick one element and check if paired up with any rem. ele = target
-        for(int i = 0; i < (len - 1) && !found; i++)
-            for(int j = i + 1; j < len && !found; j++)
+        for(int i = 0; i < nums.length; i++)
+            for(int j = i + 1; j < nums.length; j++)
                 if(nums[i] + nums[j] == target)
-                {
-                    ans[0] = i;
-                    ans[1] = j;
-                    found = true;
-                }
-        return ans;
+                    return new int[] {i, j};
+        throw new IllegalArgumentException("No two sum soln.");
     }
 }
