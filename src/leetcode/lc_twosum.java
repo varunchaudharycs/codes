f0626f04811998b33252dafd7e6a469787753f7f
Varun Chaudhary <varchd250295@gmail.com>
2019-12-21 13:45:19 -0700

    twosum

--- src/leetcode/lc_twosum.java
+++ src/leetcode/lc_twosum.java
@@ -3,13 +3,16 @@ package leetcode;
 import java.util.HashMap;
 import java.util.Map;
 
+// TIME - O(n)
+// SPACE - O(n)
+
 // https://leetcode.com/problems/two-sum/
 class lc_twosum {
     public int[] twoSum(int[] nums, int target) {
         // iterate over array putting value in hashmap(for look-back). Find the target pair(forward or backward)
         Map<Integer, Integer> hm = new HashMap<>();
         // to store remaining value -> target - current index value
-        int rem=0;
+        int rem;
         for(int i= 0; i < nums.length; i++) {
             rem = target - nums[i];
             if(hm.containsKey(rem) && hm.get(rem) != i)
