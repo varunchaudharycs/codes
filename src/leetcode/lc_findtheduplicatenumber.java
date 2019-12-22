feedd6de88665cda9225ce14fc2f82c1bd1fdf3d
Varun Chaudhary <varchd250295@gmail.com>
2019-12-22 00:25:21 -0700

    find the duplicate number

--- src/leetcode/lc_findtheduplicatenumber.java
+++ src/leetcode/lc_findtheduplicatenumber.java
@@ -5,7 +5,7 @@ import java.util.*;
 // https://leetcode.com/problems/find-the-duplicate-number/submissions/
 
 // TIME - O(n)
-// SPACE - O(n)
+// SPACE - O(n) or O(1)
 
 public class lc_findtheduplicatenumber
 {
@@ -16,6 +16,14 @@ public class lc_findtheduplicatenumber
             if(!hs.add(num))
                 return num;
         }
+    
+        // SPACE - O(1)
+//        for(int i = 0; i < nums.length; ++i) {
+//            if(nums[Math.abs(nums[i]) - 1] < 0)
+//                return Math.abs(nums[i]);
+//            else
+//                nums[Math.abs(nums[i]) - 1] = 0 - nums[Math.abs(nums[i]) - 1];
+//        }
         return -1;
     }
 }
