4e5ae320ad4cb52edfb173a7f2032c649a3185c1
Varun Chaudhary <varchd250295@gmail.com>
2019-12-25 17:20:53 -0700

    Create lc_longestsubstringwithoutrepeatingcharacters.java

---
+++ src/leetcode/lc_longestsubstringwithoutrepeatingcharacters.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.io.*;
+import java.util.*;
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+// TIME - O(n^2)
+// SPACE - O(n)
+
+class lc_longestsubstringwithoutrepeatingcharacters {
+    public int lengthOfLongestSubstring(String s) {
+        
+        HashMap<Character, Integer> hm = new HashMap<>();
+        int len_curr = 0, len_ans = 0;
+        Optional<Integer> tmp;
+        
+        for(int i = 0; i < s.length(); ++i) {
+            
+            tmp = Optional.ofNullable(hm.put(s.charAt(i), i));
+            
+            if(!tmp.isPresent())
+                len_curr++;
+            else {
+                len_ans = Math.max(len_ans, len_curr);
+                len_curr = i - tmp.get();
+                hm.clear();
+                for(int j = tmp.get(); j <= i; ++j)
+                    hm.put(s.charAt(j), j);
+            }
+        }
+        
+        return Math.max(len_ans, len_curr);
+    }
+}
