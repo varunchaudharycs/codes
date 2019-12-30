122aac33b88bc11870913a87fd37a2bd1d950b42
Varun Chaudhary <varchd250295@gmail.com>
2019-12-30 11:40:17 -0700

    Update lc_longestsubstringwithoutrepeatingcharacters.java

--- src/leetcode/lc_longestsubstringwithoutrepeatingcharacters.java
+++ src/leetcode/lc_longestsubstringwithoutrepeatingcharacters.java
@@ -9,27 +9,49 @@ import java.util.*;
 // SPACE - O(n)
 
 class lc_longestsubstringwithoutrepeatingcharacters {
+    
     public int lengthOfLongestSubstring(String s) {
         
         HashMap<Character, Integer> hm = new HashMap<>();
-        int len_curr = 0, len_ans = 0;
-        Optional<Integer> tmp;
+        int ans = 0;
         
-        for(int i = 0; i < s.length(); ++i) {
+        for(int i = 0, j = 0; j < s.length();) {
             
-            tmp = Optional.ofNullable(hm.put(s.charAt(i), i));
-            
-            if(!tmp.isPresent())
-                len_curr++;
+            if(hm.put(s.charAt(j), j) == null) {
+                ans = Math.max(ans, j - i + 1);
+                ++j;
+            }
             else {
-                len_ans = Math.max(len_ans, len_curr);
-                len_curr = i - tmp.get();
-                hm.clear();
-                for(int j = tmp.get(); j <= i; ++j)
-                    hm.put(s.charAt(j), j);
+                hm.remove(s.charAt(i));
+                ++i;
             }
         }
         
-        return Math.max(len_ans, len_curr);
+        
+        
+        return ans;
     }
+//     public int lengthOfLongestSubstring(String s) {
+        
+//         HashMap<Character, Integer> hm = new HashMap<>();
+//         int len_curr = 0, len_ans = 0;
+//         Optional<Integer> tmp;
+        
+//         for(int i = 0; i < s.length(); ++i) {
+            
+//             tmp = Optional.ofNullable(hm.put(s.charAt(i), i));
+            
+//             if(!tmp.isPresent())
+//                 len_curr++;
+//             else {
+//                 len_ans = Math.max(len_ans, len_curr);
+//                 len_curr = i - tmp.get();
+//                 hm.clear();
+//                 for(int j = tmp.get(); j <= i; ++j)
+//                     hm.put(s.charAt(j), j);
+//             }
+//         }
+        
+//         return Math.max(len_ans, len_curr);
+//     }
 }
