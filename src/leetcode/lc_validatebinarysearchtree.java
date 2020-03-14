8a6254e51070983d54d0b3ccf3911361ddb3bd52
Varun Chaudhary <varchd250295@gmail.com>
2020-03-14 14:41:51 -0700

    Create lc_validatebinarysearchtree.java

---
+++ src/leetcode/lc_validatebinarysearchtree.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/validate-binary-search-tree/
+// TIME - O(n)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class lc_validatebinarysearchtree {
+    public boolean isValidBST(TreeNode root) {
+        
+        if(root == null) return true;
+        return helper(root, null, null);
+    }
+    
+    public boolean helper(TreeNode root, Integer lower, Integer upper) {
+        
+        if(root == null) return true;
+        
+        if(lower != null && root.val <= lower) return false;
+        if(upper != null && root.val >= upper) return false;
+        // set upper bound for left sub-tree
+        boolean left_helper = helper(root.left, lower, root.val);
+        // set lower bound for right sub-tree
+        boolean right_helper = helper(root.right, root.val, upper);
+        
+        return left_helper && right_helper;
+    }
+}
