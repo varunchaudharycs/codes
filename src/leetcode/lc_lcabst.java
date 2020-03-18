9d39f51b6e2f40e6d9611ea5cc34cc1e56d11135
Varun Chaudhary <varchd250295@gmail.com>
2020-03-18 16:36:09 -0700

    Create lc_lcabst.java

---
+++ src/leetcode/lc_lcabst.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+// TIME - O(n)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class lc_lcabst {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        while(root != null) {
+            
+            if(p.val > root.val && q.val > root.val) root = root.right; // both lesser
+            else if(p.val < root.val && q.val < root.val) root = root.left; // both greater
+            else return root; // split
+        }
+        return null;
+    }
+}
