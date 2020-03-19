cb8508e54eec65fc6115ef4ea088b7e8356d8456
Varun Chaudhary <varchd250295@gmail.com>
2020-03-18 18:31:14 -0700

    Create lc_lcabinarytree.java

---
+++ src/leetcode/lc_lcabinarytree.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+// TIME - O(n)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class lc_lcabinarytree {
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        if(root == null) return null;
+        
+        if(root.val == p.val || root.val == q.val) return root;
+        
+        TreeNode left_subtree = lowestCommonAncestor(root.left, p, q);
+        TreeNode right_subtree = lowestCommonAncestor(root.right, p, q);
+        
+        if(left_subtree == null && right_subtree == null) return null;
+        if(left_subtree != null && right_subtree != null) return root;
+        
+        return (left_subtree != null) ? left_subtree : right_subtree;
+    }
+}
