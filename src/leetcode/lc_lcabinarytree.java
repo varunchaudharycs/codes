5930be0e5223f5e4fd278b295eec00400f7599db
Varun Chaudhary <varchd250295@gmail.com>
2020-03-18 18:53:24 -0700

    Update lc_lcabinarytree.java

--- src/leetcode/lc_lcabinarytree.java
+++ src/leetcode/lc_lcabinarytree.java
@@ -27,4 +27,49 @@ class lc_lcabinarytree {
         
         return (left_subtree != null) ? left_subtree : right_subtree;
     }
+    // ITERATIVE
+    
+    // Store ANCESTORS OF P & Q
+    // FIRST COMMON ANCESTORS(bottom-up) = LCA
+    
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+//         TreeNode curr;
+//         // store parents
+//         HashMap<TreeNode, TreeNode> node_parent = new HashMap<>() {{
+//             put(root, null);
+//         }}; 
+//         // store nodes
+//         Stack<TreeNode> nodes = new Stack<>() {{
+//            push(root); 
+//         }};
+//         // store all parents
+//         while(!node_parent.containsKey(p) || !node_parent.containsKey(q)) {
+//             curr = nodes.pop();
+//             // left child
+//             if(curr.left != null) {
+//                 nodes.push(curr.left);
+//                 node_parent.put(curr.left, curr);
+//             }
+//             // right child
+//             if(curr.right != null) {
+//                 nodes.push(curr.right);
+//                 node_parent.put(curr.right, curr);
+//             }
+//         }
+        
+//         HashSet<TreeNode> p_ancestors = new HashSet<>() {{
+//             add(p);
+//         }};
+//         curr = p;
+//         while(curr != null) {
+//             p_ancestors.add(node_parent.get(curr));
+//             curr = node_parent.get(curr);
+//         }
+//         curr = q;
+//         while(!p_ancestors.contains(curr)) {
+//             curr = node_parent.get(curr);
+//         }
+//         return curr;
+//     }
 }
