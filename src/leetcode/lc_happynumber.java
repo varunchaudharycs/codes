c2c4b01c928aedc6de66699b9e12cd2dce69de16
Varun Chaudhary <varchd250295@gmail.com>
2020-04-02 02:15:13 -0700

    Create lc_happynumber.java

---
+++ src/leetcode/lc_happynumber.java
@@ -0,0 +1,41 @@
+// https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/528/week-1/3284/
+// TIME - 
+package leetcode;
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+
+        Solution obj = new Solution();
+        int[] arr = {3, 6, 1, 5, 4};
+        System.out.println(obj.isHappy(18));
+    }
+
+    public int digitSquareSum(int num) {
+
+        int sum = 0;
+        while(num > 0) {
+           sum += Math.pow(num % 10, 2);
+           num /= 10;
+        }
+        return sum;
+    }
+
+    public boolean isHappy(int n) {
+
+        int count = 0; // threshold to prevent infinite loop
+        while(n != 1 && count < 1000) {
+            n = digitSquareSum(n);
+            count++;
+        } 
+            
+        if(n == 1) 
+            return true;
+        else
+            return false;
+    }
+}
