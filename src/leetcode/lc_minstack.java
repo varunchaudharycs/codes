8d344e6530a4af3cd09f084642e0e09616656e64
Varun Chaudhary <varchd250295@gmail.com>
2020-04-11 12:45:05 -0700

    Update lc_minstack.java

--- src/leetcode/lc_minstack.java
+++ src/leetcode/lc_minstack.java
@@ -6,37 +6,27 @@ import java.util.*;
 
 class lc_minstack {
     
-    int minEle;
     Stack<Integer> st;
-    /** initialize your data structure here. */
-    public lc_minstack() {
-        minEle = Integer.MAX_VALUE;
+    Stack<Integer> minst;
+
+    public lc_minstack() { //INIT
+        minst = new Stack<Integer>();
         st = new Stack<Integer>();
     }
     
     public void push(int x) {
         
-        minEle = (x < minEle) ? x : minEle;
-
         st.push(x);
+        if(minst.isEmpty()) minst.push(x);
+        else minst.push(Math.min(x, minst.peek())); // NEW MIN ?
     }
     
     public void pop() {
 
-        if(st.isEmpty()) return; // EMPTY STACK
+        if(st.isEmpty()) return;
 
-        int popEle = st.pop();
-
-        if(popEle == minEle) { // IF POPPED ELEMENT IS MIN ELE, FIND NEW MIN
-            minEle = Integer.MAX_VALUE;
-
-            Iterator it = st.iterator();
-
-            while(it.hasNext()) {
-                int ele = (int)it.next();
-                minEle = (ele < minEle) ? ele: minEle;
-            }
-        }
+        st.pop(); 
+        minst.pop();
     }
     
     public int top() {
@@ -49,17 +39,18 @@ class lc_minstack {
     
     public int getMin() {
         
-        return minEle;
+        if(!minst.isEmpty()) return minst.peek();
+        else return Integer.MAX_VALUE;
     }
 
     public static void main(String[] args)throws IOException {
         lc_minstack minStack = new lc_minstack();
-        minStack.push(-2);
         minStack.push(0);
-        minStack.push(-3);
+        minStack.push(1);
+        minStack.push(0);
         System.out.println(minStack.getMin());  // --> Returns -3.
         minStack.pop();
-        System.out.println(minStack.top());      //--> Returns 0.
+        //System.out.println(minStack.top());      //--> Returns 0.
         System.out.println(minStack.getMin());   //--> Returns -2.
     }
 }
