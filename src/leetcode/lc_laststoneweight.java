a736c27a5e689618a2e7842b8e296bddd2b66f55
Varun Chaudhary <varchd250295@gmail.com>
2020-04-12 03:38:04 -0700

    Update lc_laststoneweight.java

--- src/leetcode/lc_laststoneweight.java
+++ src/leetcode/lc_laststoneweight.java
@@ -1,5 +1,5 @@
 // https://leetcode.com/explore/featured/card/30-day-leetcoding-chllenge/529/week-2/3297/
-// TIME - O(nlogn) cuz SORTING
+// TIME - O(n), SPACE - O(max) || O(n logn), SPACE - O(n)
 package leetcode;
 import java.io.*;
 import java.util.*;
@@ -13,21 +13,55 @@ class lc_laststoneweight {
         int ans = obj.lastStoneWeight(arr);
         System.out.println(ans);
     }
-    // PRIORITY QUEUE w/ REVERSE ORDERING as PRIORITY
+    // BUCKET SORT + ELIMINATION ROUNDS
     public int lastStoneWeight(int[] stones) {
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder()); // REVERSE ORDER FOR PRIORITY
-        for(int stone : stones) pq.add(stone);
+        int len = stones.length;
+        int[] buckets = new int[Arrays.stream(stones).max().getAsInt() + 1];
 
-        while(pq.size() >= 2) {
+        for(int weight : stones) buckets[weight]++;
 
-            int max1 = pq.poll();
-            int max2 = pq.poll();
+        int pointer = Arrays.stream(stones).max().getAsInt();
 
-            if(max1 > max2) pq.add(max1 - max2); // IF MAX > 2nd MAX
+        while(pointer > 0) {
+
+            if((buckets[pointer] & 1) == 0) { // EVEN COUNT, DESTROYED IN PAIRS
+                buckets[pointer] = 0; 
+            }
+            else { // ODD COUNT, FIND COMPETITIOR FOR SURVIVOR
+                boolean found = false;
+                for(int competitor = pointer - 1; competitor >= 0; competitor--) {
+
+                    if(buckets[competitor] > 0) {
+                        found = true;
+                        buckets[competitor]--;
+                        buckets[pointer] = 0;
+                        if(pointer > competitor) buckets[pointer - competitor]++; // MAX1 - MAX2
+                        pointer = competitor;
+                        break;
+                    }
+                }
+                if(!found) break; // NOT FOUND (last remaining weight)
+            }
+            pointer--;
         }
-
-        return (pq.size() == 0) ? 0 : pq.poll();          
+        return pointer;
     }
+    // PRIORITY QUEUE w/ REVERSE ORDERING as PRIORITY
+//     public int lastStoneWeight(int[] stones) {
+
+//         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder()); // REVERSE ORDER FOR PRIORITY
+//         for(int stone : stones) pq.add(stone);
+
+//         while(pq.size() >= 2) {
+
+//             int max1 = pq.poll();
+//             int max2 = pq.poll();
+
+//             if(max1 > max2) pq.add(max1 - max2); // IF MAX > 2nd MAX
+//         }
+
+//         return (pq.size() == 0) ? 0 : pq.poll();          
+//     }
 }
 
