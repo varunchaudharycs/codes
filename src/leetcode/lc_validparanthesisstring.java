be8d983557b0eb9769e41988305b871324397633
Varun Chaudhary <varchd250295@gmail.com>
2020-04-16 14:58:47 -0700

    Update lc_validparanthesisstring.java

--- src/leetcode/lc_validparanthesisstring.java
+++ src/leetcode/lc_validparanthesisstring.java
@@ -20,39 +20,66 @@ class lc_validparanthesisstring {
             System.out.println(ans);
         }
     }
+    // TRACK MAX & MIN OPEN "("
+    // COMPENSATE FOR "*" being "(" OR ")" OR NULL
+    public boolean checkValidString(String s) {
+        
+        int minOpen = 0, maxOpen = 0;
+        
+        for(char c : s.toCharArray()) {
+            
+            if(c == '(') { 
+                minOpen++;
+                maxOpen++;
+            }
+            else if(c == ')') { // MATCHES w/ ")"
+                minOpen--;
+                maxOpen--;
+            }
+            else { // "*" CAN BE "(" OR ")"
+                minOpen--;
+                maxOpen++;
+            }
+            
+            if(maxOpen < 0) return false; // INVALID, ")" w/o A MATCH
+            
+            minOpen = Math.max(minOpen, 0); // COMPENSATE FOR "*" BEING NULL
+        }
+        return (minOpen == 0) ? true : false;
+    }
     // For ")" = Check for "(" before lifeline "*"
     // For each leftover "(", there should be a "*" ON ITS RIGHT(need to save position/indices)
-    public boolean checkValidString(String s) {
+//     public boolean checkValidString(String s) {
 
-        Stack<Integer> lifelineIdx = new Stack<>(); // STORE "(" INDICES
-        Stack<Integer> openIdx = new Stack<>(); // STORE "*" INDICES
+//         Stack<Integer> lifelineIdx = new Stack<>(); // STORE "(" INDICES
+//         Stack<Integer> openIdx = new Stack<>(); // STORE "*" INDICES
 
-        for(int i = 0; i < s.length(); i++) {
+//         for(int i = 0; i < s.length(); i++) {
 
-            char c = s.charAt(i);
+//             char c = s.charAt(i);
 
-            if(c == '*')
-                lifelineIdx.push(i);
-            else if(c == '(')
-                openIdx.push(i);
-            else if(c == ')') {
-                if(!openIdx.isEmpty()) // LOOK FOR "(" FIRST
-                  openIdx.pop();
-                else if(!lifelineIdx.isEmpty()) // NONE? LIFELINE SAVES
-                  lifelineIdx.pop();
-                else 
-                  return false; // INVALID
-            }
-        }
-        if(openIdx.isEmpty()) // NO OPEN "(" LEFT
-            return true; 
-        else { // FOR EACH LEFTOVER "(" => A "*" ON ITS RIGHT
-            while(!openIdx.isEmpty() && !lifelineIdx.isEmpty()) {
-                if(openIdx.pop() > lifelineIdx.pop()) return false;
-            }
-            if(openIdx.isEmpty()) return true;
-            else return false; // INVALID
-        }
+//             if(c == '*')
+//                 lifelineIdx.push(i);
+//             else if(c == '(')
+//                 openIdx.push(i);
+//             else if(c == ')') {
+//                 if(!openIdx.isEmpty()) // LOOK FOR "(" FIRST
+//                   openIdx.pop();
+//                 else if(!lifelineIdx.isEmpty()) // NONE? LIFELINE SAVES
+//                   lifelineIdx.pop();
+//                 else 
+//                   return false; // INVALID
+//             }
+//         }
+//         if(openIdx.isEmpty()) // NO OPEN "(" LEFT
+//             return true; 
+//         else { // FOR EACH LEFTOVER "(" => A "*" ON ITS RIGHT
+//             while(!openIdx.isEmpty() && !lifelineIdx.isEmpty()) {
+//                 if(openIdx.pop() > lifelineIdx.pop()) return false;
+//             }
+//             if(openIdx.isEmpty()) return true;
+//             else return false; // INVALID
+//         }
     }
 }
 
