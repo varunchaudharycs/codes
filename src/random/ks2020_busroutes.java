c4e09640a276892793bd450129e44157840eae96
Varun Chaudhary <varchd250295@gmail.com>
2020-04-18 16:47:24 -0700

    Create ks2020_busroutes.java

---
+++ src/random/ks2020_busroutes.java
@@ -0,0 +1,35 @@
+// https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ffc8/00000000002d83bf
+// TIME - O(n)
+
+//package random;
+import java.io.*;
+import java.util.*;
+
+public class ks2020_busroutes {
+
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(buf.readLine());
+
+        for(int test = 1; test <= t; test++ ) {
+
+            String[] srr = buf.readLine().split(" ");
+            int n = Integer.parseInt(srr[0]);
+            long d = Long.parseLong(srr[1]);
+            srr = buf.readLine().split(" ");
+            int[] routes = new int[n];
+            for(int i = 0; i < n; i++) routes[i] = Integer.parseInt(srr[i]);
+
+            long departure = d; // GO FROM END & WAIT TILL BUS AVAILABLE
+            for(int i = n - 1; i >= 0; i--) {
+                departure -= (departure % routes[i]);
+            }
+           
+            System.out.println("Case #" + test + ": " + departure);
+        }
+    }
+
+    
+}
