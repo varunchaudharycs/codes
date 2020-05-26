dc52a088d6355dd1f998bf59df9b2e61d50cd9f9
Varun Chaudhary <varchd250295@gmail.com>
2020-05-26 08:08:46 -0700

    Create cf_parklighting.java
    codeforces contest #1358

---
+++ src/codeforces/cf_parklighting.java
@@ -0,0 +1,40 @@
+// https://codeforces.com/contest/1358/problem/A
+// TIME - O(1)
+
+package leetcode;
+import java.io.*;
+import java.util.*;
+
+public class cf_parklighting {
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(buf.readLine());
+
+		while((t--) > 0) {
+
+			StringTokenizer st = new StringTokenizer(buf.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+
+			int ans = 0;
+
+			if((n & 1) == 1 && (m & 1) == 1) {
+				ans = ((n - 1)/2 * m) + ((int)Math.ceil(m/2) + 1);
+			}
+
+			else {
+				if((n & 1) == 1) {
+					ans = m/2 * n;
+				}
+				else {
+					ans = n/2 * m;
+				}
+			}
+
+			System.out.println(ans);
+		}
+	}
+}
