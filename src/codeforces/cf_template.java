baacec3cd9fbb90ce92abacb3266df43ad080bb1
Varun-Github <varunchaudharycs@gmail.com>
2020-06-07 11:39:25 -0700

    codeforces #1365

---
+++ src/codeforces/cf_template.java
@@ -0,0 +1,49 @@
+// link
+// TIME - O()
+
+//package codeforces;
+import java.io.*;
+import java.util.*;
+
+public class cf_template {
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+
+
+		int testCases = Integer.parseInt(buf.readLine());
+
+		while((testCases--) > 0) {
+
+			String s;
+			StringTokenizer st;
+			int n;
+
+			s = buf.readLine();
+			st = new StringTokenizer(s);
+			n = Integer.parseInt(st.nextToken());
+
+			int[] arr = new int[n];
+			st = new StringTokenizer(buf.readLine());
+
+			for(int i = 0; i < n; ++i) {
+
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+
+			System.out.println(getWinner(arr, n));
+
+		}
+
+
+	}
+
+	// logic
+	public static int getAnswer(int[] arr, int n) {
+
+		return -1;
+	}
+}
+
+// I/O
