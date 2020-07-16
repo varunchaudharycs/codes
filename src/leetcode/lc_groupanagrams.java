49ea9953df8becaea71711310a11c41dee2c40a1
Varun Chaudhary <varchd250295@gmail.com>
2020-07-15 18:42:10 -0700

    Update lc_groupanagrams.java

--- src/leetcode/lc_groupanagrams.java
+++ src/leetcode/lc_groupanagrams.java
@@ -21,27 +21,42 @@ public class lc_groupanagrams {
                 System.out.print("word - " + s + ",");
         }
     }
-    // (key : value) => (alphabet#Count : List of strings)
+    // encodes string as alphabetCount, sorted alphabetically. eg. tea => a1e1t1
+     public static String getCode(String s) {
+        
+        s = s.toLowerCase();
+        int len = s.length();
+        int[] alpha = new int[26];
+        
+        for(int i = 0; i < len; ++i) {
+            
+            alpha[s.charAt(i) - 'a']++;
+        }
+        
+        StringBuilder code = new StringBuilder();
+        
+        for(int i = 0; i < 26; ++i) 
+            if(alpha[i] != 0)
+                code.append('a' + "" + i + alpha[i]);
+        
+        return code.toString();
+    }
+    // Encode all strings. Anagrams get same code/key in map of (code) -> (strings)
     public List<List<String>> groupAnagrams(String[] strs) {
         
-        HashMap<String, List<String>> anagrams = new HashMap<>();
-        int[] alphabets = new int[26];
-
-        for(String str : strs) {
-
-            Arrays.fill(alphabets, 0); // RESET ALPHABET COUNTS
-            String newStr = "";
-
-            for(Character c : str.toCharArray()) alphabets[c - 'a']++; // COUNT ALPHABETS
-
-            for(int i = 0; i < 26; i++) // PREPARE CODE
-                if(alphabets[i] > 0)
-                    newStr += Character.toString((char)'a' + i) + alphabets[i];
-
-            if(!anagrams.containsKey(newStr)) // INSERT IN MAP
-                 anagrams.put(newStr, new ArrayList<String>());
+        HashMap<String, List<String>> hm = new HashMap<>();
+        int len = strs.length;
+        List<List<String>> anagrams = new ArrayList<>();
+        
+        for(int i = 0; i < len; ++i) {
+            
+            hm.computeIfAbsent(getCode(strs[i]), k -> new ArrayList<String>()).add(strs[i]);
         }
-        return new ArrayList<>(anagrams.values()); // GENERATE OUTPUT FORMAT              
+        
+        for(Map.Entry<String, List<String>> e : hm.entrySet())
+            anagrams.add(e.getValue());
+            
+        return anagrams;
     }
     // (key : value) => (Sorted String : List of strings)
 //     public List<List<String>> groupAnagrams(String[] strs) {
